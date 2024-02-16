00a946afc456b2d0e54d292505f3c87d8bc8c3a6
vismayrathore123 <HOME@DESKTOP-EAS70TG>
2024-02-16 14:28:22 +0530

    Add project files.

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        List<Category> listofcategories = new List<Category>() { 
+        new Category{Id=1, Title="Samsung",DisplayOder=1},
+          new Category{Id=2, Title="Nokia",DisplayOder=2},
+            new Category{Id=3, Title="Apple",DisplayOder=3},
+              new Category{Id=4, Title="RealMe",DisplayOder=4},
+                new Category{Id=5, Title="Oppo",DisplayOder=5},
+            };
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return listofcategories;
+        }
+
+    }
+}
---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DisplayOder { get; set; }
+    }
+}
